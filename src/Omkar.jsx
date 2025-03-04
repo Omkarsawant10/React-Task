cefc5f122fc1d5443383bd4485250b81321e44bd
Omkarsawant10 <122078907+Omkarsawant10@users.noreply.github.com>
2025-03-04 23:44:04 +0530

    first commit

---
+++ src/Omkar.jsx
@@ -0,0 +1,68 @@
+// export const Omkar=()=>{
+//     return (
+//         <>
+//            <Car/>
+//            <Bike/>
+//         </>
+//     )
+// }
+
+// const Car=()=> <h1>This is car</h1>
+
+// const Bike=()=> <h1>This is bike</h1>
+
+// export const Omkar=()=>{
+//     return(
+//         <>
+//            <Person/>
+//            <Message/>
+//         </>
+//     )
+// }
+
+// const Person=()=>{
+//     return(
+//         <h1>This is person</h1>
+//     )
+// }
+
+// const Message=()=>{
+//     return(
+//         <h1>This is message</h1>
+//     )
+// }
+export const Omkar=()=>{
+    return(
+        <>
+            <Person/>
+        </>
+    )
+}
+
+const Person=()=>{
+    return(
+        <>
+            <h1>Name:Omkar</h1>
+            <h1>Age:20</h1>
+            <h1>Location:Pune</h1>
+            <p>Message:<Message/></p>
+            <Img/>
+        </>
+    )
+}
+
+const Message=()=>{
+    return(
+        <>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates minus dicta sequi aliquam perspiciatis recusandae incidunt, laborum provident ducimus quo ad tempore quas fugit quaerat sunt nam est rem. Quis!
+        </>
+    )
+}
+
+const Img=()=>{
+    return(
+        <>
+            <img src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B08345R1ZW._SY116_CB667322346_.jpg'" alt="" />
+        </>
+    )
+}
---
+++ src/Test.jsx
@@ -0,0 +1,7 @@
+export const Test=()=>{
+    return(
+        <>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis officiis beatae neque consequuntur sed! Numquam id velit cum maiores magnam, est aperiam minus similique voluptatibus, fuga molestias reprehenderit! Unde, doloremque.
+        </>
+    )
+}
